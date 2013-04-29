6b4e7bf73501aea42598271238cae0c9accd3918
harpreetsingh <harpreet@cloudbees.com>
2013-04-29 16:33:16 +0530

    Minimized tests and partialized routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
+
   describe "Home page" do
-    
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-    
-    it "should have the base title'" do
-      visit '/static_pages/home'
-      page.should have_title('RoR Tutorial Sample App')
-    end
+    before {visit root_path}
+    it { should have_selector('h1', text: 'Sample App') }
+
+    it { should have_title('RoR Tutorial Sample App') }
   end
   
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    before {visit help_path}
+
+    it { should have_content('Help') }
   end
 
   describe "About Us" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
+    before {visit about_path}
+
+    it { should have_content('About Us') }
+
   end
-  
-  
+
+  describe "Contact" do
+    before {visit contact_path}
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title('Contact') }
+
+  end
+
+
 end
