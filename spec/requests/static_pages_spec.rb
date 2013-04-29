16e73c2580863baf6563f9d44423f4092cc72188
harpreetsingh <harpreet@cloudbees.com>
2013-04-29 09:49:17 +0530

    Fixed Capybara 2.0 incompatibility change. have_selector replaced by has_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    
+    it "should have the base title'" do
+      visit '/static_pages/home'
+      page.should have_title('RoR Tutorial Sample App')
     end
   end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "About Us" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+  
+  
 end
